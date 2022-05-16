9a73e4b91069396d042329ff8695ed1ba9c93a12
BellaBradbury <bellabradbury@yahoo.com>
2022-05-16 18:00:44 -0400

    STEP TWO
    added const variables for start game button to hide overlay & added hide overlay function
    
    POSSIBLE ERRORS

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const keyrow = document.getElementById('qwerty')
-const phrase = document.getElementById('phrase')
+const keyrow = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const startButton = document.getElementsByClassName('btn_reset');
 let missed = 0;
+
+startButton.addEventListner('submit', () => {
+  const overlay = document.getElementById('overlay');
+  overlay.style.display = 'none';
+});
