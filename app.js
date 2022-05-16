8a4eaad9fa9d888ce8f60f065448c5300ad60ff4
BellaBradbury <bellabradbury@yahoo.com>
2022-05-16 18:07:08 -0400

    STEP FOUR
    phrase array added to APP.JS with 5 common phrases
    
    POSSIBLE ERRORS

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const keyrow = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
+const phraseDiv = document.getElementById('phrase');
 const startButton = document.getElementsByClassName('btn_reset');
+
+const phrases = [
+  'liar liar pants on fire',
+  'the cow jumped over the moon',
+  'life is just a bowl of cherries',
+  'a game of cat and mouse',
+  'when life gives you lemons make lemonade'
+];
 let missed = 0;
 
 startButton.addEventListner('submit', () => {
